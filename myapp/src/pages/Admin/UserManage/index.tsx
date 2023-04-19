bb62cdb9efe0668773dace37cb1c4e179e462dbf
Ray7788 <ray778@foxmail.com>
2023-04-19 08:29:43 +0100

    Add admin option

---
+++ myapp/src/pages/Admin/UserManage/index.tsx
@@ -0,0 +1,174 @@
+import Footer from '@/components/Footer';
+import {register} from '@/services/ant-design-pro/api';
+// import { getFakeCaptcha } from '@/services/ant-design-pro/UserManage';
+import {LockOutlined, UserOutlined,} from '@ant-design/icons';
+import {LoginForm, ProFormText,} from '@ant-design/pro-components';
+import {message, Tabs} from 'antd';
+import React, {useState} from 'react';
+import {FormattedMessage, history, SelectLang, useIntl} from 'umi';
+import styles from './index.less';
+import {PLANET_LINK, SYSTEM_LOGO} from "@/constants";
+
+const Register: React.FC = () => {
+  const [type, setType] = useState<string>('account');
+  const intl = useIntl();
+
+  // 表单提交
+  const handleSubmit = async (values: API.RegisterParams) => {
+    const { userPassword, checkPassword} = values;
+    if (userPassword != checkPassword){
+      message.error('两次输入密码不一致');
+      return;
+    }
+    try {
+      // 注册
+      const id = await register (values);
+      if (id > 0) {
+        const defaultLoginSuccessMessage = intl.formatMessage({
+          id: 'pages.login.success',
+          defaultMessage: '注册成功！',
+        });
+        message.success(defaultLoginSuccessMessage);
+        /** 此方法会跳转到 redirect 参数所在的位置 */
+        if (!history) return;
+        const { query } = history.location;
+        // const { query } = query as { redirect: string };
+        history.push({
+          pathname: 'user/login',
+          query,
+        });
+          // '/usr/login?redirect' + redirect);
+        return;
+      }else {
+        throw new Error('register error id = ${id}');
+      }
+
+      // 如果失败去设置用户错误信息
+      // setUserLoginState(user);
+    } catch (error) {
+      const defaultLoginFailureMessage = intl.formatMessage({
+        id: 'pages.login.failure',
+        defaultMessage: '注册失败，请重试！',
+      });
+      message.error(defaultLoginFailureMessage);
+    }
+  };
+
+  return (
+    <div className={styles.container}>
+      <div className={styles.lang} data-lang>
+        {SelectLang && <SelectLang />}
+      </div>
+      <div className={styles.content}>
+        <LoginForm
+          submitter={{
+            searchConfig:{
+              submitText:'注册'
+            }
+          }}
+          logo={<img alt="logo" src={SYSTEM_LOGO} />}
+          title="User Center 用户中心"
+          subTitle={<a href={PLANET_LINK} target="_blank" rel="noreferrer"> 世界上第二好的用户中心</a>}
+          initialValues={{
+            autoLogin: true,
+          }}
+
+          onFinish={async (values) => {
+            await handleSubmit(values as API.RegisterParams);
+          }}
+        >
+          <Tabs activeKey={type} onChange={setType}>
+            <Tabs.TabPane
+              key="account"
+              tab={intl.formatMessage({
+                id: 'pages.login.accountLogin.tab',
+                defaultMessage: '账号密码注册',
+              })}
+            />
+          </Tabs>
+          {type === 'account' && (
+            <>
+              <ProFormText
+                name="userAccount"
+                fieldProps={{
+                  size: 'large',
+                  prefix: <UserOutlined className={styles.prefixIcon} />,
+                }}
+                placeholder={intl.formatMessage({
+                  id: 'pages.login.username.placeholder',
+                  defaultMessage: '账号 Your Account',
+                })}
+                rules={[
+                  {
+                    required: true,
+                    message: (
+                      <FormattedMessage
+                        id="pages.login.username.required"
+                        defaultMessage="请输入账号!"
+                      />
+                    ),
+                  },
+                ]}
+              />
+              <ProFormText.Password
+                name="userPassword"
+                fieldProps={{
+                  size: 'large',
+                  prefix: <LockOutlined className={styles.prefixIcon} />,
+                }}
+                placeholder={intl.formatMessage({
+                  id: 'pages.login.password.placeholder',
+                  defaultMessage: '请输入密码 Type in Password',
+                })}
+                rules={[
+                  {
+                    required: true,
+                    message: (
+                      <FormattedMessage
+                        id="pages.login.password.required"
+                        defaultMessage="密码是必填项！"
+                      />
+                    ),
+                  },
+                  {
+                    min:8,
+                    type:'string',
+                    message:'长度不能小于8',
+                  },
+                ]}
+              />
+              <ProFormText.Password
+                name="checkPassword"
+                fieldProps={{
+                  size: 'large',
+                  prefix: <LockOutlined className={styles.prefixIcon} />,
+                }}
+                placeholder= {'请确认密码'}
+                rules={[
+                  {
+                    required: true,
+                    message: (
+                      <FormattedMessage
+                        id="pages.login.password.required"
+                        defaultMessage="密码是必填项！"
+                      />
+                    ),
+                  },
+                  {
+                    min:8,
+                    type:'string',
+                    message:'长度不能小于8',
+                  },
+                ]}
+              />
+            </>
+          )}
+
+        </LoginForm>
+      </div>
+      <Footer />
+    </div>
+  );
+};
+
+export default Register;
